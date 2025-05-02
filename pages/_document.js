5b5063db060a2be9cfe2146df37bd4cbb3536fe5
v1fingameon <v1@fingameon.com>
2025-05-02 18:49:33 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -22,13 +22,18 @@ export default function Document() {
             })(window,document,'script','dataLayer','GTM-TFN2TW78');
           `,
         }} />
-  <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "rda3ngyjbf");
-</script>
+  <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rda3ngyjbf");
+            `,
+          }}
+        />
       </Head>
 
       <body>
