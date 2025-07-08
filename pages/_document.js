013dac83807e13879ee67a46ebc07e53c91d8269
v1fingameon <v1@fingameon.com>
2025-07-08 12:09:42 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="robots" content="noindex">
         <meta charSet="utf-8" />
         <link rel="icon" href="/resources/Diamond.png" />
         <link rel="stylesheet" type="text/css" />
