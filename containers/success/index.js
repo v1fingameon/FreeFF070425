61efdec5a79f95e68fdea369cbc9a3c7e588896b
v1fingameon <v1@fingameon.com>
2025-07-26 10:56:02 +0530

    Update index.js

--- containers/success/index.js
+++ containers/success/index.js
@@ -23,51 +23,6 @@ function Success() {
             data-full-width-responsive="true" />
         </div>
         <div className="flex flex-col gap-6 items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="77" height="77" viewBox="0 0 77 77" fill="none">
-            <g filter="url(#filter0_d)">
-              <ellipse cx="38.3289" cy="38.4065" rx="29.387" ry="28.6702" fill="url(#paint0_linear)" />
-            </g>
-            <path
-              d="M26.3843 37.9236L34.4517 46.0005L50.615 29.8467"
-              stroke="white"
-              strokeWidth="3.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <circle
-              cx="38.5"
-              cy="38.5"
-              r="37.5"
-              stroke="url(#paint1_linear)"
-              strokeLinecap="round"
-              strokeDasharray="4 4"
-            />
-            <defs>
-              <filter id="filter0_d" x="4.94189" y="5.73633" width="66.7739" height="65.3408" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                <feOffset />
-                <feGaussianBlur stdDeviation="2" />
-                <feComposite in2="hardAlpha" operator="out" />
-                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
-                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
-                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
-              </filter>
-              <linearGradient id="paint0_linear" x1="-4.26876" y1="-12.4179" x2="66.5689" y2="-24.0447" gradientUnits="userSpaceOnUse">
-                <stop offset="0.00939447" stopColor="#FE652D" />
-                <stop offset="1" stopColor="#FF8A26" />
-              </linearGradient>
-              <linearGradient id="paint1_linear" x1="-15.8578" y1="-27.9773" x2="74.6508" y2="-42.4704" gradientUnits="userSpaceOnUse">
-                <stop offset="0.00939447" stopColor="#FE652D" />
-                <stop offset="1" stopColor="#FF8A26" />
-              </linearGradient>
-            </defs>
-          </svg>
-
-          <div className="text-white text-xl font-semibold text-center">
-            We will review it and <br /> update within 30 minutes.
-          </div>
-
           <div className="flex flex-col gap-4 w-full">
             <a href='/'>
               <button className="flex items-center justify-center py-2.5 rounded-xl w-full bg-gradient-to-r from-purple-600 to-blue-400">
